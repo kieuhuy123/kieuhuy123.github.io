3fb45830cb3b7bc0e07cd4fc7a12037dfccee2ba
Huy <kieuphuchuy2002@gmail.com>
2021-07-30 02:11:54 +0700

    demo4

---
+++ finalproject/js/product-detail.js
@@ -0,0 +1,8 @@
+// Thay doi src cua img
+
+document.querySelectorAll(".product-img-item").forEach((e) => {
+  e.addEventListener("click", () => {
+    let img = e.querySelector("img").getAttribute("src");
+    document.querySelector("#product-img > img").setAttribute("src", img);
+  });
+});

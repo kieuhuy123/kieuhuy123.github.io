a885b2ab56f0941e335fdcdb80a3a77ddd8eb6cd
Huy <kieuphuchuy2002@gmail.com>
2021-08-12 15:57:04 +0700

    demo7

--- finalproject/js/product-detail.js
+++ finalproject/js/product-detail.js
@@ -1,12 +1,3 @@
-// Thay doi src cua img
-
-// document.querySelectorAll(".product-img-item").forEach((e) => {
-//   e.addEventListener("click", () => {
-//     let img = e.querySelector("img").getAttribute("src");
-//     document.querySelector("#product-img > img").setAttribute("src", img);
-//   });
-// });
-
 // View all
 document
   .querySelector("#view-all-description")
--- finalproject/js/product-list.js
+++ finalproject/js/product-list.js
@@ -5,6 +5,7 @@ let products = [
     image2: "./img/nam.jpg",
     old_price: "1,200,000 VND",
     curr_price: "990,00 VND",
+    shape: "round",
   },
   {
     name: "Hardy",
@@ -12,6 +13,7 @@ let products = [
     image2: "./img/nam2.jpg",
     old_price: "1,750,000 VND",
     curr_price: "990,00 VND",
+    shape: "rec",
   },
   {
     name: "Uno",
@@ -19,6 +21,7 @@ let products = [
     image2: "./img/nu2.jpg",
     old_price: "2,550,00VND",
     curr_price: "1,750,000 VND",
+    shape: "square",
   },
   {
     name: "Ena",
@@ -26,6 +29,7 @@ let products = [
     image2: "./img/nu.jpg",
     old_price: "1,200,000 VND",
     curr_price: "990,00 VND",
+    shape: "round",
   },
   {
     name: "Kubo",
@@ -33,6 +37,7 @@ let products = [
     image2: "./img/marke-3.jpg",
     old_price: "1,750,000 VND",
     curr_price: "990,00 VND",
+    shape: "cat",
   },
   {
     name: "Fade",
@@ -40,37 +45,41 @@ let products = [
     image2: "./img/fade-3.jpg",
     old_price: "1,200,000 VND",
     curr_price: "990,00 VND",
+    shape: "square",
   },
-  // {
-  //   name: "Arthur",
-  //   image1: "./img/arthur-1.jpg",
-  //   image2: "./img/arthur-2.jpg",
-  //   old_price: "1,750,000 VND",
-  //   curr_price: "1,200,000 VND",
-  // },
-  // {
-  //   name: "Ben",
-  //   image1: "./img/ben-1.jpg",
-  //   image2: "./img/ben-3.jpg",
-  //   old_price: "1,200,000 VND",
-  //   curr_price: "990,000 VND",
-  // },
-  // {
-  //   name: "Oli",
-  //   image1: "./img/oli-1.jpg",
-  //   image2: "./img/oli-2.jpg",
-  //   old_price: "2,550,000 VND",
-  //   curr_price: "1,750,000 VND",
-  // },
+  {
+    name: "Arthur",
+    image1: "./img/arthur-1.jpg",
+    image2: "./img/arthur-2.jpg",
+    old_price: "1,750,000 VND",
+    curr_price: "1,200,000 VND",
+    shape: "rec",
+  },
+  {
+    name: "Ben",
+    image1: "./img/ben-1.jpg",
+    image2: "./img/ben-3.jpg",
+    old_price: "1,200,000 VND",
+    curr_price: "990,000 VND",
+    shape: "rec",
+  },
+  {
+    name: "Oli",
+    image1: "./img/oli-1.jpg",
+    image2: "./img/oli-2.jpg",
+    old_price: "2,550,000 VND",
+    curr_price: "1,750,000 VND",
+    shape: "round",
+  },
 ];
 let product_list = document.querySelector("#products");
 
 renderProducts = (products) => {
   products.forEach((e) => {
     let prod = `
-<div class="col-12 col-md-6 col-xxl-4">
-            <div class="product-card">
-              <div class="product-card-img">
+<div class="col-12 col-md-6 col-xxl-4 itemBox ${e.shape}">
+            <div class="product-card  ">
+              <div class="product-card-img ">
                 <img src="${e.image1}" alt="" />
                 <img src="${e.image2}" alt="" />
               </div>

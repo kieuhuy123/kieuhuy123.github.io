dc228d9c24725ee527a63eef7017b817a31c2982
Huy <kieuphuchuy2002@gmail.com>
2021-08-17 00:45:10 +0700

    demo 10.4

--- finalproject/js/app.js
+++ finalproject/js/app.js
@@ -5,9 +5,20 @@ document.querySelectorAll(".mega-dropdown > a").forEach((e) => {
   e.addEventListener("click", (event) => event.preventDefault());
 });
 
-document.querySelector("#mb-menu-toggle").addEventListener("click", () => {
-  document.querySelector(".header-wrapper").classList.add("active");
-});
-document.querySelector("#mb-menu-close").addEventListener("click", () => {
-  document.querySelector(".header-wrapper").classList.remove("active");
-});
+// document.querySelector("#mb-menu-toggle").addEventListener("click", () => {
+//   document.querySelector(".header-wrapper").classList.add("active");
+// });
+// document.querySelector("#mb-menu-close").addEventListener("click", () => {
+//   document.querySelector(".header-wrapper").classList.remove("active");
+// });
+
+
+// 
+let filter_col = document.querySelector("#filter-col");
+
+// document.querySelector("#filter-toggle").addEventListener("click", () => {
+//   filter_col.classList.add("active");
+// });
+// document.querySelector("#filter-close").addEventListener("click", () => {
+//   filter_col.classList.remove("active");
+// });

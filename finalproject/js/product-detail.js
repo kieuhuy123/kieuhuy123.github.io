dc923891ec77cbf14ea4fc7c5349d95725275a18
Huy <kieuphuchuy2002@gmail.com>
2021-08-21 18:23:20 +0700

    demo 11.0

--- finalproject/js/product-detail.js
+++ finalproject/js/product-detail.js
@@ -1,14 +1,14 @@
 // View all
-document
-  .querySelector("#view-all-description")
-  .addEventListener("click", () => {
-    let content = document.querySelector(".product-detail-content");
+// document
+//   .querySelector("#view-all-description")
+//   .addEventListener("click", () => {
+//     let content = document.querySelector(".product-detail-content");
 
-    content.classList.toggle("active");
+//     content.classList.toggle("active");
 
-    document.querySelector("#view-all-description").innerHTML =
-      content.classList.contains("active") ? "Đọc thêm" : "Ẩn bớt";
-  });
+//     document.querySelector("#view-all-description").innerHTML =
+//       content.classList.contains("active") ? "Đọc thêm" : "Ẩn bớt";
+//   });
 
 // const swiper = new Swiper(".swiper-container", {
 //   zoom: {

ff87099b5a5b16c333d14cd3531d70b47d974665
kieuhuy123 <80501882+kieuhuy123@users.noreply.github.com>
2021-11-12 15:59:32 +0700

    Update main.js

--- React-Js/redux-lab9/src/main.js
+++ React-Js/redux-lab9/src/main.js
@@ -8,7 +8,7 @@ import {
 } from "./actions/actionType";
 
 /*=== COUNTER-SLICE ===*/
-stdore.subscribe(renderCounter);
+store.subscribe(renderCounter);
 
 const countEl = document.getElementById("count");
 const btnIncrement = document.getElementById("increment");

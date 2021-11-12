cf2c6b437bd7853c43b2ab6d91232f0a16121311
kieuhuy123 <80501882+kieuhuy123@users.noreply.github.com>
2021-11-12 15:51:21 +0700

    Delete React-Js/react-lab9 directory

--- React-Js/react-lab9/src/actions/actionType.js
+++
@@ -1,7 +0,0 @@
-const INCREMENT = "counter/increment";
-const DECREMENT = "counter/decrement";
-
-const ADD_TODO = "todo/addNewTodo";
-const REMOVE_TODO = "todo/removeTodo";
-
-export { INCREMENT, DECREMENT, ADD_TODO, REMOVE_TODO };

92aedbf7b52e9b552e06853aff31f93be8a446d0
kieuhuy <kieuphuchuy2002@gmail.com>
2021-11-16 18:19:19 +0700

    React-web

---
+++ react-website-demo/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Homepage/Home";
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home}></Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;

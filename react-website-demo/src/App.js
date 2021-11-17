cd703e615567ada7e100d53dca9d67162663402e
kieuhuy <kieuphuchuy2002@gmail.com>
2021-11-17 13:54:07 +0700

    new web

--- react-website-demo/src/App.js
+++ react-website-demo/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import Home from './pages/HomePage/Home'
 import Navbar from "./components/Navbar";
 function App() {
   return (
     <Router>
       <Navbar/>
       <Switch>
-        <Route path="/"></Route>
+        <Route path="/" exact component={Home}></Route>
       </Switch>
       
     </Router>
---
+++ react-website-demo/src/pages/HomePage/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import HeroSection from "../../components/HeroSection";
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from "./Data";
+
+function Home() {
+  return (
+    <>
+      <HeroSection {...homeObjOne} />
+      <HeroSection {...homeObjThree} />
+      <HeroSection {...homeObjTwo} />
+      <HeroSection {...homeObjFour} />
+    </>
+  );
+}
+
+export default Home;

1c36cf73eddd6b2fe254dae73cf56a54f1255a05
kieuhuy <kieuphuchuy2002@gmail.com>
2021-11-18 23:38:39 +0700

    update web

--- react-website-demo/src/App.js
+++ react-website-demo/src/App.js
@@ -3,14 +3,17 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './pages/HomePage/Home'
 import Navbar from "./components/Navbar";
+import Footer from "./pages/Footer/Footer"
 function App() {
   return (
     <Router>
       <Navbar/>
+      
       <Switch>
         <Route path="/" exact component={Home}></Route>
       </Switch>
-      
+
+      <Footer></Footer>
     </Router>
   );
 }
--- react-website-demo/src/pages/HomePage/Home.js
+++ react-website-demo/src/pages/HomePage/Home.js
@@ -6,8 +6,8 @@ function Home() {
   return (
     <>
       <HeroSection {...homeObjOne} />
-      <HeroSection {...homeObjThree} />
       <HeroSection {...homeObjTwo} />
+      <HeroSection {...homeObjThree} />
       <HeroSection {...homeObjFour} />
     </>
   );
